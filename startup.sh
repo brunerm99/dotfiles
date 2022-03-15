6605604be1cb16e92939aaf285ee9022971d40f1
Marshall Bruner <brunerm99@gmail.com>
2022-03-15 12:32:57 -0600

    xorg and cleaned startup

--- startup.sh
+++ startup.sh
@@ -3,9 +3,10 @@
 DOTFILES=$HOME/dotfiles
 
 # Create conf directories if they don't exist
-mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.config/polybar
-mkdir -p $HOME/.config/i3
+DIRS=("nvim" "polybar" "i3")
+for DIR in ${DIRS[@]}; do
+	mkdir -p $HOME/.config/$DIR
+done
 
 # Link config files
 ln -sf $DOTFILES/nvim/init.lua $HOME/.config/nvim/init.lua
@@ -13,3 +14,4 @@ ln -sf $DOTFILES/zsh/.zshrc $HOME/.zshrc
 ln -sf $DOTFILES/i3/config $HOME/.config/i3/config
 ln -sf $DOTFILES/polybar/config.ini $HOME/.config/polybar/config.ini
 ln -sf $DOTFILES/polybar/launch.sh $HOME/.config/polybar/launch.sh
+ln -sf $DOTFILES/xorg/.xinitrc $HOME/.xinitrc
