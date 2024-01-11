676ae956790613b6a58aeb2361614b97e883aec1
Marshall Bruner <brunerm99@gmail.com>
2024-01-11 11:09:24 +0759

    Use system clipboard

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -2,6 +2,7 @@ vim.api.nvim_command('set number')
 vim.api.nvim_command('set relativenumber')
 vim.api.nvim_command('set cursorline')
 vim.api.nvim_command('set smartindent')
+vim.api.nvim_command('set clipboard=unnamedplus')
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
