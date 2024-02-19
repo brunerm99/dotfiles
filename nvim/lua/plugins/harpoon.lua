f3cdc3de57ebbe6ac113104adda7d8128b421ba2
Marshall Bruner <brunerm99@gmail.com>
2024-02-19 11:39:57 -0700

    Update zellij config to work with vim bindings; update harpoon bindings to not use CTRL

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,8 +6,8 @@ return {
     require("harpoon"):setup()
   end,
   keys = {
-    { "<C-S-H>", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Harpoon toggle quick menu", },
-    { "<C-S-N>", function() require("harpoon"):list():next() end, desc = "Harpoon to next file", },
-    { "<C-S-I>", function() require("harpoon"):list():prev() end, desc = "Harpoon to previous file", },
+    { "<leader>hh", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Harpoon toggle quick menu", },
+    { "<leader>hi", function() require("harpoon"):list():next() end, desc = "Harpoon to next file", },
+    { "<leader>hn", function() require("harpoon"):list():prev() end, desc = "Harpoon to previous file", },
   },
 }
