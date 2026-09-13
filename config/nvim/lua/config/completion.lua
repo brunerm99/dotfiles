8d85f20bda2d09a2acf773ad11e306739b9b3599
Marshall Bruner <marshall@makerain.com>
2026-09-13 10:43:27 -0600

    Add filesystem path completion to Neovim

--- config/nvim/lua/config/completion.lua
+++ config/nvim/lua/config/completion.lua
@@ -21,6 +21,7 @@ cmp.setup({
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
   },
   sources = {
+    { name = "path", option = { trailing_slash = true } },
     { name = "nvim_lsp" },
   },
   window = {
